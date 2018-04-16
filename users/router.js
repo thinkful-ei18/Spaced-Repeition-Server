be24df6bebbdf43bb8c26324a8aba06a736305e2
admin <Muaath.Alaraj@gmail.com>
2018-04-16 11:13:27 -0700

    Add Lastname and Firstname to schema

--- users/router.js
+++ users/router.js
@@ -8,79 +8,80 @@ const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
-router.post('/', jsonParser, (req,res) => {
+router.post('/', jsonParser, (req, res) => {
   console.log('legeo');
   const requiredFields = ['username', 'password'];
-  const missingField = requiredFields.find((field) => !(field in req.body));
+  const missingField = requiredFields.find(field => !(field in req.body));
 
-  if (missingField){
+  if (missingField) {
     return res.status(422).json({
-      code:422,
+      code: 422,
       reqson: 'ValidationError',
       message: 'MissingField',
-      location: missingField
+      location: missingField,
     });
   }
 
   const stringFields = ['username', 'password'];
   const nonStringField = stringFields.find(
-    (field) => field in req.body && typeof req.body[field] !== 'string'
+    field => field in req.body && typeof req.body[field] !== 'string'
   );
 
-  if (nonStringField){
+  if (nonStringField) {
     return res.status(422).json({
-      code:422,
+      code: 422,
       reqson: 'ValidationError',
       message: 'Incorrect Type',
-      location: nonStringField
+      location: nonStringField,
     });
   }
 
-  const explicitTrimmedFields = ['username','password'];
+  const explicitTrimmedFields = ['username', 'password'];
   const nonTrimmedFeild = explicitTrimmedFields.find(
-    (field) => req.body[field].trim() !== req.body [field]
+    field => req.body[field].trim() !== req.body[field]
   );
 
-  if (nonTrimmedFeild){
+  if (nonTrimmedFeild) {
     return res.status(422).json({
-      code:422,
+      code: 422,
       reqson: 'ValidationError',
       message: 'Cannot start or end with whitespace',
-      location: nonTrimmedFeild
+      location: nonTrimmedFeild,
     });
   }
 
-  let {username, password} = req.body;
+  let { username, password, firstName, lastName } = req.body;
 
-  return User.find({username})
+  return User.find({ username })
     .count()
-    .then((count) => {
-      if(count > 0){
+    .then(count => {
+      if (count > 0) {
         return Promise.reject({
-          code:422,
+          code: 422,
           reason: 'ValidationError',
           message: 'Username taken',
-          location: 'username'
+          location: 'username',
         });
       }
       return User.hashPasword(password);
     })
-    .then((hash) => {
+    .then(hash => {
       return User.create({
         username,
         password: hash,
+        lastName,
+        firstName,
       });
     })
-    .then((user) => {
+    .then(user => {
       return res.status(201).json(user.serialize());
     })
-    .catch((err) => {
-      if (err.reason === 'ValidationError'){
+    .catch(err => {
+      if (err.reason === 'ValidationError') {
         return res.status(err.code).json(err);
       }
-      res.status(500).json({code: 500, message: 'Internal server error'});
+      res.status(500).json({ code: 500, message: 'Internal server error' });
     });
-
 });
 
 module.exports = { router };
